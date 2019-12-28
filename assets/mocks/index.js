534c2ccbef593817203cdaadde1f624a8806fae5
Angelito <angel.vasquez@neximo.mx>
2019-12-27 18:15:55 -0600

    add assets

---
+++ assets/mocks/index.js
@@ -0,0 +1,255 @@
+export const states = [
+  {
+    name: 'CDMX',
+    address: 'Ciudad de México',
+    id: '09',
+    locationType: 'state',
+    image: require('assets/images/states/Mexico_City.jpg')
+  },
+  {
+    name: 'Nayarit',
+    address: 'Nayarit',
+    id: '18',
+    locationType: 'state',
+    image: require('assets/images/states/nayarit.jpg')
+  },
+  {
+    name: 'Puebla',
+    address: 'Puebla',
+    id: '21',
+    locationType: 'state',
+    image: require('assets/images/states/Puebla.jpg')
+  },
+  {
+    name: 'Tlaxcala',
+    address: 'Tlaxcala',
+    id: '29',
+    locationType: 'state',
+    image: require('assets/images/states/Tlaxcala.jpg')
+  },
+  {
+    name: 'Durango',
+    address: 'Durango',
+    id: '10',
+    locationType: 'state',
+    image: require('assets/images/states/Durango.jpg')
+  },
+  {
+    name: 'Tabasco',
+    address: 'Tabasco',
+    id: '27',
+    locationType: 'state',
+    image: require('assets/images/states/Tabasco.jpg')
+  },
+  {
+    name: 'Zacatecas',
+    address: 'Zacatecas',
+    id: '32',
+    locationType: 'state',
+    image: require('assets/images/states/Zacatecas.jpg')
+  },
+  {
+    name: 'Jalisco',
+    address: 'Jalisco',
+    id: '14',
+    locationType: 'state',
+    image: require('assets/images/states/Jalisco.jpg')
+  },
+  {
+    name: 'Baja California',
+    address: 'Baja California',
+    id: '02',
+    locationType: 'state',
+    image: require('assets/images/states/Baja_California.jpg')
+  },
+  {
+    name: 'Coahuila de Zaragoza',
+    address: 'Coahuila de Zaragoza',
+    id: '05',
+    locationType: 'state',
+    image: require('assets/images/states/Coahuila_de_Zaragoza.jpeg')
+  },
+  {
+    name: 'Baja California Sur',
+    address: 'Baja California Sur',
+    id: '03',
+    locationType: 'state',
+    image: require('assets/images/states/Baja_California_Sur.jpg')
+  },
+  {
+    name: 'Chihuahua',
+    address: 'Chihuahua',
+    id: '08',
+    locationType: 'state',
+    image: require('assets/images/states/Chihuahua.jpg')
+  },
+  {
+    name: 'Colima',
+    address: 'Colima',
+    id: '06',
+    locationType: 'state',
+    image: require('assets/images/states/Colima.jpg')
+  },
+  {
+    name: 'Nuevo León',
+    address: 'Nuevo León',
+    id: '19',
+    locationType: 'state',
+    image: require('assets/images/states/Nuevo_Leon.jpg')
+  },
+
+  {
+    name: 'Oaxaca',
+    address: 'Oaxaca',
+    id: '20',
+    locationType: 'state',
+    image: require('assets/images/states/Oaxaca.jpg')
+  },
+  {
+    name: 'Michoacán de Ocampo',
+    address: 'Michoacán de Ocampo',
+    id: '16',
+    locationType: 'state',
+    image: require('assets/images/states/Michoacan_de_Ocampo.jpeg')
+  },
+  {
+    name: 'Sinaloa',
+    address: 'Sinaloa',
+    id: '25',
+    locationType: 'state',
+  },
+  {
+    name: 'Sonora',
+    address: 'Sonora',
+    id: '26',
+    locationType: 'state',
+    image: require('assets/images/states/Sonora.jpg')
+  },
+  {
+    name: 'Veracruz de Ignacio de la Llave',
+    address: 'Veracruz de Ignacio de la Llave',
+    id: '30',
+    locationType: 'state',
+    image: require('assets/images/states/Veracruz.jpg')
+  },
+  {
+    name: 'Guanajuato',
+    address: 'Guanajuato',
+    id: '11',
+    locationType: 'state',
+    image: require('assets/images/states/Guanajuato.jpg')
+  },
+  {
+    name: 'Aguascalientes',
+    address: 'Aguascalientes',
+    id: '01',
+    locationType: 'state',
+    image: require('assets/images/states/Aguascalientes.jpg')
+  },
+  {
+    name: 'Campeche',
+    address: 'Campeche',
+    id: '04',
+    locationType: 'state',
+    image: require('assets/images/states/Campeche.jpeg')
+  },
+  {
+    name: 'San Luis Potosí',
+    address: 'San Luis Potosí',
+    id: '24',
+    locationType: 'state',
+    image: require('assets/images/states/San_Luis_Potosi.jpg')
+  },
+  {
+    name: 'Tamaulipas',
+    address: 'Tamaulipas',
+    id: '28',
+    locationType: 'state',
+    image: require('assets/images/states/Tamaulipas.jpg')
+  },
+
+  {
+    name: 'Yucatán',
+    address: 'Yucatán',
+    id: '31',
+    locationType: 'state',
+    image: require('assets/images/states/Yucatan.jpg')
+  },
+  {
+    name: 'México',
+    address: 'México',
+    id: '15',
+    locationType: 'state',
+    image: require('assets/images/states/Mexico.jpg')
+  },
+  {
+    name: 'Hidalgo',
+    address: 'Hidalgo',
+    id: '13',
+    locationType: 'state',
+    image: require('assets/images/states/Hidalgo.jpg')
+  },
+  {
+    name: 'Querétaro',
+    address: 'Querétaro',
+    id: '22',
+    locationType: 'state',
+    image: require('assets/images/states/Queretaro.jpg')
+  },
+  {
+    name: 'Chiapas',
+    address: 'Chiapas',
+    id: '07',
+    locationType: 'state',
+    image: require('assets/images/states/Chiapas.jpg')
+  },
+  {
+    name: 'Morelos',
+    address: 'Morelos',
+    id: '17',
+    locationType: 'state',
+    image: require('assets/images/states/Morelos.jpg')
+  },
+  {
+    name: 'Quintana Roo',
+    address: 'Quintana Roo',
+    id: '23',
+    locationType: 'state',
+    image: require('assets/images/states/Quintana_Roo.jpg')
+  },
+  { name: 'Guerrero', address: 'Guerrero', id: '12', locationType: 'state' }
+].map(state => ({ image: require('assets/images/cancun_cover.jpg'), ...state }))
+
+export const betterInMexico = [
+  {
+    name: 'Yucatán',
+    id: 'yucatan',
+    image:
+      'https://hablemosdeculturas.com/wp-content/uploads/2018/11/piramides_mayas_2.png'
+  },
+  {
+    name: 'Chiapas',
+    address: 'Chiapas',
+    id: '07',
+    locationType: 'state',
+    image:
+      'https://www.elitetraveler.com/wp-content/uploads/2012/12/best_Pyramids1-462x346.jpg'
+  },
+  {
+    name: 'Tabasco',
+    address: 'Tabasco',
+    id: '27',
+    locationType: 'state',
+    image:
+      'https://www.sheknows.com/wp-content/uploads/2018/08/Top-4-places-Mexico.jpg_znxvnf.jpeg'
+  },
+  {
+    name: 'Querétaro',
+    address: 'Querétaro',
+    id: '22',
+    locationType: 'state',
+    image: 'https://globetotting.com/wp-content/uploads/2017/11/Puebla.jpg'
+  },
+  { name: 'Guerrero', address: 'Guerrero', id: '12', locationType: 'state' }
+]
+
